fe1b2719940d356975dd7b56d53380842ebd9508
raineer <delaritaraineer81@gmail.com>
2019-05-24 22:24:49 +0800

    image upload data working

--- routes/blog.js
+++ routes/blog.js
@@ -4,6 +4,32 @@ const express = require("express");
 
 const Blog = require("../models/blog");
 
+const multer = require("multer");
+
+const cloudinary = require("cloudinary");
+
+const storage = multer.diskStorage({
+  filename(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+const imageFilter = (req, file, cb) => {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage, fileFilter: imageFilter });
+
+cloudinary.config({
+  cloud_name: "dwsbpkgvr",
+  api_key: "246382268158277",
+  api_secret: "OEJwFk8xMOuNID7Z7L5MNDJ9nY8"
+});
+
 const router = express.Router();
 
 // router
@@ -41,18 +67,29 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-  new Blog({
-    title: req.body.title,
-    content: req.body.content,
-    image_url: req.body.image_url,
-    created_at: req.body.created_at,
-    updated_at: req.body.updated_at
-  })
-    .save()
-    .then(function(blog) {
-      return res.send({ blog });
-    });
+router.post("/", upload.single("image"), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, result => {
+    // add cloudinary url for the image to the object under image property
+    console.log(result.secure_url);
+    // console.log("req.body,", req.body);
+
+    //res.json(result);
+    new Blog({
+      title: req.body.title,
+      content: req.body.content,
+      image_url: result.secure_url,
+      created_at: req.body.created_at,
+      updated_at: req.body.updated_at
+    })
+      .save()
+      .then(function(blog) {
+        return res.json({
+          ok: true,
+          blog,
+          message: "Saved"
+        });
+      });
+  });
 });
 
 router
