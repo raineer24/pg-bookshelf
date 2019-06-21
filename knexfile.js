f54ef8fefa67d0a6b807ddc0483824864c2b3ea9
raineer <delaritaraineer81@gmail.com>
2019-06-22 01:33:05 +0800

    migrations folder

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,11 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/db/migrations`
+    }
   }
 };
 
