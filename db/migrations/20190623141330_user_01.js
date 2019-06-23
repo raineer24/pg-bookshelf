e17119b273baf45dbea668755a7def089734efc7
raineer <delaritaraineer81@gmail.com>
2019-06-23 18:34:18 +0800

    unique email

--- db/migrations/20190623141330_user_01.js
+++ db/migrations/20190623141330_user_01.js
@@ -10,7 +10,8 @@ exports.up = function(knex, Promise) {
       table
         .text("email")
         .defaultTo("")
-        .nullable();
+        .nullable()
+        .unique();
       table.text("first_name").notNullable();
       table.text("last_name").notNullable();
       table
@@ -35,5 +36,38 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.resolve().then(() => knex.schema.dropTable("useraccount"));
+  return Promise.resolve().then(() =>
+    knex.schema.createTable("useraccount", table => {
+      table
+        .increments("id")
+        .primary()
+        .notNullable();
+      table.text("username").notNullable();
+      table.text("password").notNullable();
+      table
+        .text("email")
+        .defaultTo("")
+        .nullable()
+        .unique();
+      table.text("first_name").notNullable();
+      table.text("last_name").notNullable();
+      table
+        .text("uiid")
+        .defaultTo("")
+        .nullable();
+      table
+        .text("gender")
+        .defaultTo("")
+        .nullable();
+      table
+        .text("mobile_number")
+        .defaultTo("")
+        .nullable();
+      table.date("birth_date").nullable();
+      table.string("deactivated", 1).defaultTo("0");
+      table.string("forced_reset", 1).defaultTo("0");
+      table.datetime("created_at").notNullable();
+      table.datetime("updated_at").notNullable();
+    })
+  );
 };
