0e61f7fafcc5269b680be3abc345b4311261e0a1
raineer <delaritaraineer81@gmail.com>
2019-06-23 15:54:24 +0800

    delete post 02

--- db/migrations/20190621214235_modify_time10.js
+++ db/migrations/20190621214235_modify_time10.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.raw(`
-    ALTER TABLE blogs 
+    ALTER TABLE "blogs" 
 ADD created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE blogs ADD updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
   `);
