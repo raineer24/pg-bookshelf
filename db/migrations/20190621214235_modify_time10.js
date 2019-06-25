3ef710c90774b36dd9bc75421a9305c7ba6313d6
raineer <delaritaraineer81@gmail.com>
2019-06-25 15:20:07 +0800

    email delete type unique

--- db/migrations/20190621214235_modify_time10.js
+++ db/migrations/20190621214235_modify_time10.js
@@ -1,15 +1,15 @@
 exports.up = function(knex, Promise) {
   return knex.raw(`
-    ALTER TABLE "blogs" 
-ADD created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE blogs ADD updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
+    ALTER TABLE blogs 
+ADD created_at TIMESTAMPTZ;
+ALTER TABLE blogs ADD updated_at TIMESTAMPTZ;
   `);
 };
 
 exports.down = function(knex, Promise) {
   return knex.raw(`
-   ALTER TABLE "blogs"
-ALTER TABLE blogs ADD created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE blogs ADD updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP;
+   ALTER TABLE blogs
+ALTER TABLE blogs ADD created_at TIMESTAMPTZ;
+ALTER TABLE blogs ADD updated_at TIMESTAMPTZ;
   `);
 };
